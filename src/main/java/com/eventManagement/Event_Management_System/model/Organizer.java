56a927d1a1e841e971abb90565a18b12aa6674bb
sandesh300 <bhujbalsandesh52@gmail.com>
2024-08-02 20:31:31 +0530

    create model of event, organizer and venue

---
+++ src/main/java/com/eventManagement/Event_Management_System/model/Organizer.java
@@ -0,0 +1,58 @@
+package com.eventManagement.Event_Management_System.model;
+
+
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Organizer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String contactInfo;
+
+    @OneToMany(mappedBy = "organizer")
+    private Set<Event> events;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(Set<Event> events) {
+        this.events = events;
+    }
+
+    public Organizer(){
+
+    }
+}
