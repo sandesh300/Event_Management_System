d102054aa4cdbfff48975eff4dad24bec44207f6
sandesh300 <bhujbalsandesh52@gmail.com>
2024-08-02 20:36:54 +0530

    create event repository

---
+++ src/main/java/com/eventManagement/Event_Management_System/repository/EventRepository.java
@@ -0,0 +1,12 @@
+package com.eventManagement.Event_Management_System.repository;
+
+
+
+import com.eventManagement.Event_Management_System.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByVenueId(Long venueId);
+    List<Event> findByOrganizerId(Long organizerId);
+}
